54912c605d6bc6812ae0c0b9e46d095673bf0d6c
Michael Mansperger <ManspergerMichael@gmail.com>
2019-05-31 15:11:58 -0700

    strings

--- Variables.php
+++ Variables.php
@@ -1,4 +1,13 @@
 <?php
+//variable names are declared with $name
 $first_name = "Michael";
 echo $first_name;
+//string concatination with .
+echo "My name is " . $first_name . ".";
+//or not
+echo "$first_name don't need . for concatination"
+//double quotes are for strings that need variables and special charactors
+//single quotes are read literaly
+
+
 ?>
