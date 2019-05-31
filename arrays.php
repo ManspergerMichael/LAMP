04d2d77ab94a414ce18a614c3a61b611a619fc9c
Michael Mansperger <ManspergerMichael@gmail.com>
2019-05-31 16:14:46 -0700

    me funny

--- arrays.php
+++ arrays.php
@@ -3,5 +3,10 @@ $arrayOne = array(4,'poop',true);
 //or
 $arrayTwo = [];
 
+//var dump displays information about the variable
 var_dump($arrayOne);
+
+//push
+array_push($arrayOne, "pee");
+echo $arrayOne[3];
 ?>
