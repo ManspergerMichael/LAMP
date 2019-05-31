2362fe7269f53038c41b2070dfe904e3249b847b
Michael Mansperger <ManspergerMichael@gmail.com>
2019-05-31 14:44:40 -0700

    echo and print

--- Basics.php
+++ Basics.php
@@ -1,3 +1,10 @@
+
 <?php
-echo "Hello World"
+// all php code must go in between the php tags
+
+//echo and print do the same thing but echo is faster and takes more arguments
+//include HTML for cleaner output
+echo "<h3>Hello World</h3>";
+print "<h1>hi planet</h1>";
+echo "Fuck off print no one likes you";
 ?>
