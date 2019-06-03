6c943bbb673da30c23709e4d98b11fff5a977985
Michael Mansperger <ManspergerMichael@gmail.com>
2019-06-03 13:45:41 -0700

    loops and odd numbers

--- Loops.php
+++ Loops.php
@@ -1,9 +1,14 @@
 <?php 
 //forloop
-    for($i = 0; $i <= 10; $i++) {
+    /* for($i = 0; $i <= 10; $i++) {
         echo "Hello World! <br>";
-    }
+    } */
 
-    
+    $users = array('Mike', 'Jeffrey');
+
+    foreach($users as $user)
+    {
+        echo "$user<br>";
+    }
     
 ?>
