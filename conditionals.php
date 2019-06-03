32637efb91df59d9b4957842b1982aeb31e2b4ad
Michael Mansperger <ManspergerMichael@gmail.com>
2019-06-03 13:15:17 -0700

    conditionals

---
+++ conditionals.php
@@ -0,0 +1,16 @@
+<?php 
+    $subject = "English";
+    var_dump($subject == 'Math');
+
+    echo "<br> conditionals<br>";
+
+    
+    if($subject == 'Math') {
+        echo "The subject is Math";
+    } else {
+        echo "The subject is English";
+    }
+
+    echo "<br> var dump is pushed to the bottom of the content.<br>"
+
+?>
