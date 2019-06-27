664aac8efef23d7275de6a2efc41a245ee9a0fcc
Michael Mansperger <ManspergerMichael@gmail.com>
2019-06-27 11:35:50 -0700

    multiples of 5

--- PHPFundimentals/MoreFunctions.php
+++ PHPFundimentals/MoreFunctions.php
@@ -3,9 +3,9 @@ $array = array(1,4,8,8);
 
 function MultipleOf5($arr)
 {
-   foreach ($arr as $key) 
+   for($i = 0; $i < count($arr); $i++)
    {
-       
+       $arr[$i] = $arr[$i] * 5;
    }
 
    echo "<p>Multiple of 5 result</p> <ul>";
