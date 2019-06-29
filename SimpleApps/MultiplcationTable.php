2ef5b3f0a2d9c65d057e8c03fdfac4ba71e8a90c
Michael Mansperger <ManspergerMichael@gmail.com>
2019-06-28 18:15:59 -0700

    Multiplication table start

--- SimpleApps/MultiplcationTable.php
+++ SimpleApps/MultiplcationTable.php
@@ -9,6 +9,32 @@
     <script src="main.js"></script>
 </head>
 <body>
+    <div class="container">
     <h1>Multiplcation Table</h1>
+        <table>
+            <?php 
+            for($i = 1; $i <= 10; $i++)
+            {
+                echo "<tr>";
+                for($j = 1; $j <=10; $j++)
+                {
+                    $ii = $i * $j;
+                    if($j % 2 == 0)
+                    {
+                        echo "<td class='even'>$ii</td>";
+                    }
+                    else
+                    {
+                        echo "<td class='odd'>$ii</td>";
+                    }
+                    
+                }
+                echo "</tr>";
+            }
+            ?>
+        </table>
+    </div>
 </body>
 </html>
+
+
